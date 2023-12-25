aefa114183e31db29e06e9be6f1265a9c8bd3323
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-12-25 13:53:38 +0200

    refactor - save to db (to fix error

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Express from "express";
 import {createServer} from "http";
 import {Server} from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
+import db from "./db";
 
 const app = Express();
 const httpServer = createServer(app);
@@ -15,6 +16,51 @@ app.get("/",(_, res)=>{
     res.send("re");
 });
 
+async function saveMessage(message: string, connectionId: string, apiKey: string, isAgent: boolean){
+  
+    //find project to update
+    const targetProject = await db.project.findUnique({
+        where:{
+            api_key: apiKey,
+        },
+    });
+    if(!targetProject) return;
+
+   const conversationExists = await db.conversation.findUnique({
+    where:{
+        connectionId,
+    },
+   });
+
+   if(!conversationExists){
+    //create conversation
+    await db.conversation.create({
+        data:{
+            connectionId,
+            projectId: targetProject.id,
+        },
+    });
+    await db.message.create({
+        data:{
+            message,
+            isAgent,
+            messagedAt: new Date(Date.now()).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }),
+            conversationId: connectionId,
+        },
+    });
+   }else{
+    //update conversation
+    await db.message.create({
+        data:{
+            message,
+            isAgent,
+            messagedAt: new Date(Date.now()).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }),
+            conversationId: connectionId,
+        },
+    })
+   }
+}
+
 io.on("connection",(socket)=>{
     console.log("new connection", socket.id, socket.handshake.query);
     //@ts-ignore to solve .trim()
@@ -26,6 +72,8 @@ io.on("connection",(socket)=>{
         socket.join(connectionId);
         
         socket.on("message", (message)=>{
+            //save message to db
+            saveMessage(message.message,connectionId,socket.handshake.query.apiKey as string,false);
             socket.to(socket.handshake.query.apiKey!).emit("DingloClient-DashboardMessage", {...message, connectionId});
             socket.emit("message_client",message);
         });
@@ -36,7 +84,7 @@ io.on("connection",(socket)=>{
         socket.join(socket.handshake.query.id!);
         
         socket.on("DingloServer-DashboardMessage",(msg)=>{
-            console.log("message de la client", msg);
+            saveMessage(msg.message,msg.connectionId,socket.handshake.query.id as string,true);
             socket.to(msg.connectionId).emit("message_client",msg);
         })
     }
